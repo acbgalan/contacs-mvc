b7506c1d679cf956642e4c61ed28447e8546a928
Carlos Blanco <acbgalan@gmail.com>
2024-02-16 10:13:54 +0100

    Added contracts for repositories

---
+++ src/Contacts.DataAccess/Repository/Contracts/IRepositoryAsync.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contacts.DataAccess.Repository.Contracts
+{
+    public interface IRepositoryAsync<T> where T : class
+    {
+        Task AddAsync(T entity);
+        Task<T> GetAsync(int id);
+        Task<List<T>> GetAllAsync();
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+        Task DeleteASync(T entity);
+        Task<bool> ExitsASync(int id);
+        Task<int> SaveAsync();
+    }
+}
