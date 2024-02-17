5e83e7c81ca59310969ffd327c5aad84f756e34f
Carlos Blanco <acbgalan@gmail.com>
2024-02-17 01:54:32 +0100

    EF seeding migration

---
+++ src/Contacts.DataAccess/Migrations/20240217005146_SeedGroupData.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Contacts.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedGroupData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Groups",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Contactos de emergencia" },
+                    { 2, "Amigos" },
+                    { 3, "Empresa" },
+                    { 4, "Familia" },
+                    { 5, "Otros" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Groups",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Groups",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Groups",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Groups",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Groups",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
