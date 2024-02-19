7ae817eb674e91fab846ed2d5727e899dd31ad7a
Carlos Blanco <acbgalan@gmail.com>
2024-02-19 09:58:59 +0100

    Readme.md updated, link to contacts added in navbar and index for contact controller

---
+++ src/Contacts.Web/Controllers/ContactController.cs
@@ -0,0 +1,22 @@
+﻿using Contacts.DataAccess.Repository.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Contacts.Web.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IUnitOfWork _uof;
+
+        public ContactController(IUnitOfWork unitOfWork)
+        {
+            _uof = unitOfWork;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var contacts = await _uof.ContactRepository.GetAllAsync();
+
+            return View(contacts);
+        }
+    }
+}
