d791d842ca6e6222404530b65da08cdf8fb256b1
Carlos Blanco <acbgalan@gmail.com>
2024-02-19 10:58:01 +0100

    Fix SaveAsync method and validation style for groups

--- src/Contacts.Web/Controllers/GroupController.cs
+++ src/Contacts.Web/Controllers/GroupController.cs
@@ -27,7 +27,7 @@ namespace Contacts.Web.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Group group)
+        public async Task<IActionResult> Create([FromBody] Group group)
         {
             if (!ModelState.IsValid)
             {
@@ -35,7 +35,7 @@ namespace Contacts.Web.Controllers
             }
 
             await _uow.GroupRepository.AddAsync(group);
-            int saveResult = await _uow.GroupRepository.SaveAsync();
+            int saveResult = await _uow.SaveAsync();
 
             if (saveResult > 0)
             {
